c7a590dcc3173487720bd45b02328dacdba5f396
Sandeep <you@example.com>
2026-05-05 14:59:23 +0530

    fix login request format

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -18,17 +18,18 @@ function Login({ setToken }) {
     setError("");
 
     try {
-      const params = new URLSearchParams();
-      params.append("username", email);
-      params.append("password", password);
+     const params = new URLSearchParams();
+     params.append("username", emailOrUsername);  // use your input variable
+     params.append("password", password);
 
-      axios.post("https://inventra-eaht.onrender.com/login",
-        params,
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        }
+     const response = await axios.post(
+     "https://inventra-eaht.onrender.com/login",
+      params,
+      {
+         headers: {
+         "Content-Type": "application/x-www-form-urlencoded",
+      },
+     }
       );
 
       localStorage.setItem("token", response.data.access_token);
